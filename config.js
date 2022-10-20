7ef1dacd120719a1a570e5a2b8e9546b057fad54
zuu31 <108630296+zuu31@users.noreply.github.com>
2022-10-20 14:21:26 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'iu';
+const giftUrl = 'https://www.facebook.com/hanigawcutene';
+const eventName = 'Happy Womens Day 20-10';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'hehe chúc emiu cụa anh 20-10 zui zẻ thặc xink địp và iu anh hơn iu em nhèo lắm tuy ko có hoa nhưng mà có hoa background nhé';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'https://scontent.xx.fbcdn.net/v/t1.15752-9/312214069_3459689534276483_7486008815028892852_n.jpg?stp=dst-jpg_s206x206&_nc_cat=105&ccb=1-7&_nc_sid=aee45a&_nc_ohc=DNwU_n_55XMAX_HdBcA&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdQTj4rRBjavI4yBa98qfRnDZC-ZR3b369UFayLwCDGt7Q&oe=63761DCE';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
